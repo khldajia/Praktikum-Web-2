7d48bdc958ba778abe52d3c4448f3365df48a831
khldajia <kholidajiaf11@gmail.com>
2023-06-05 19:28:26 +0700

    Tugas Praktikum 11

--- praktikum10/app/Http/Controllers/ProdukController.php
+++ praktikum10/app/Http/Controllers/ProdukController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class ProdukController extends Controller
@@ -13,7 +14,11 @@ class ProdukController extends Controller
      */
     public function index()
     {
-        return view('produk.produk');
+        // jalanin fungsi getAllProduk
+        $produk = Produk::getAllProduk();
+
+        // kirim data
+        return view('produk.produk', compact('produk'));
     }
 
     /**
